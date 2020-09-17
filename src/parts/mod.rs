9f09a48c46d58688a6a3088226212177f469503c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-09-17 01:21:55 +0100

    Added materials and local optical properties.

--- src/parts/mod.rs
+++ src/parts/mod.rs
@@ -4,18 +4,9 @@ pub mod attributes;
 pub mod key;
 // pub mod light;
 // pub mod light_builder;
-// pub mod local;
-// pub mod material;
-// pub mod material_builder;
+pub mod local;
+pub mod material;
+pub mod material_builder;
 // pub mod photon;
 
-pub use self::{attributes::*, key::*};
-// pub use self::{
-// attributes::*,
-// light::*,
-// light_builder::*,
-// local::*,
-// material::*,
-// material_builder::*,
-// photon::*,
-// };
+pub use self::{attributes::*, key::*, local::*, material::*, material_builder::*};
