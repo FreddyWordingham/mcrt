f689dd84c5033a5d6a7cd5f19ed0a1b3252457ed
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-16 04:17:17 +0000

    Added clean version.

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo doc --all-features;
+cargo doc --all-featur;
 rm -r docs;
 mv target/doc docs;
 echo "<meta http-equiv=refresh content=0;url=mcrt/index.html>" > docs/index.html;
